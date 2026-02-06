3fd029e9c40450723e9016ddb382ce27fce1b938
Dmaina5054 <dmain5054@gmail.com>
2026-02-06 05:14:57 +0300

    chore: Remove scripts for generating and copying Kubernetes certificates and keys. This is the hard way

--- copy_cert_pk_wn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for host in worker-0 worker-1; do
- ssh root@${host} mkdir /var/lib/kubelet
- scp ca.crt root@${host}:/var/lib/kubelet
- scp ${host}.crt root@${host}:/var/lib/kubelet/kubelet.crt
- scp ${host}.key root@${host}:/var/lib/kubelet/kubelet/key
-done
